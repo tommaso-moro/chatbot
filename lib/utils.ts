a8d7cfa39e9679cf8751f8362430a34b6127e10c
tommaso-moro <tomms1998@gmail.com>
2023-10-09 22:13:14 +0100

    added links

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const LINKS = {
+  GITHUB: "https://github.com/tommaso-moro/chatbot",
+  MISTRAL_7B: "https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.1",
+  VERCEL: "https://vercel.com",
+  TMORO: "https://www.tmoro.xyz",
+};
