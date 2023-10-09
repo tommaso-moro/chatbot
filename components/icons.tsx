444afbac3c944bc9f09a42e6bd68f3a1a5763cdd
tommaso-moro <tomms1998@gmail.com>
2023-10-09 16:04:23 +0100

    added icons

---
+++ components/icons.tsx
@@ -0,0 +1,9 @@
+import { Moon, SunMedium, GithubIcon, Send } from "lucide-react";
+
+export const Icons = {
+  //todo order alphabetically
+  sun: SunMedium,
+  moon: Moon,
+  github: GithubIcon,
+  send: Send,
+};
