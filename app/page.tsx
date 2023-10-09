58df6760a968ce47dc8b26bad0e9a6a0fa496231
tommaso-moro <tomms1998@gmail.com>
2023-10-09 23:45:47 +0100

    reduced padding bottom

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatbotCard from "@/components/chatbot-card";
 
 export default function Chat() {
   return (
-    <div className="p-2 md:p-32 flex items-center justify-center h-screen">
+    <div className="p-2 md:p-32 md:pb-6 flex items-center justify-center h-screen">
       <div className="w-full lg:w-4/5">
         <ChatbotCard />
       </div>
